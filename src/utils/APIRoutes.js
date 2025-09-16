b8921be0ef7da59dca5b3cdc1e4d5303571b28e8
sushmasingh2000 <sushmasingh16166@gmail.com>
2025-09-16 10:56:34 +0530

    jhfg

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,7 +1,6 @@
 // export const domain = 'http://192.168.18.214:2001';
 export const domain = 'https://edgefx.uk';
 export const frontend = 'https://edgefx.uk';
-// export const frontend = 'http://localhost:3000';
 export const dollar = "$"
 
 export const endpoint = {
